0886457da1b1b8cfa22e08771769e116c9a520fe
Sammy-Barasa <kesammybarasa@gmail.com>
2023-01-24 23:08:28 +0300

    compiled opcode segment0 to 4 bit binary

--- assembler.py
+++ assembler.py
@@ -263,11 +263,71 @@ class Assembler():
 				code = lines[i][j]
 				line_compiled = line_compiled +" "+code
 
-			# change line to half-bytes [[opcode][R1][R2][R3][PC][COND][U][L]]
+		# change line to half-bytes [[opcode][R1][R2][R3][PC][COND][U][L]]
+		instruction = [[],[],[],[],[],[],[],[]]
+		# print(self.all_lines_info)
+		for j in range(0,len(lines)):
+			if lines[j][0] == '0x00':
+				start_memory = self.all_lines_info[str((j-1))]["start"]
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+				instruction[4]=f"{eval(start_memory):04b}"
+
+			elif lines[j][0] == '0x01':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+
+			elif lines[j][0] == '0x02':
+				# li R1 0x00000000
+				start_memory = self.all_lines_info[str((j+1))]["start"]
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+				instruction[4]=f"{eval(start_memory):04b}"
+				if lines[j][1] == 'R1':
+					instruction[1]=f"{eval(lines[j][2]):04b}"
+				elif lines[j][1] == 'R2':
+					instruction[2]=f"{eval(lines[j][2]):04b}"
+				elif lines[j][1] == 'R3':
+					instruction[3]=f"{eval(lines[j][2]):04b}"
+				
+			elif lines[j][0] == '0x03':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+			
+			elif lines[j][0] == '0x04':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+
+			elif lines[j][0] == '0x05':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+			
+			elif lines[j][0] == '0x06':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+			
+			elif lines[j][0] == '0x07':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+
+			elif lines[j][0] == '0x08':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+			
+			elif lines[j][0] == '0x09':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+			
+			elif lines[j][0] == '0x0A':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+			
+			elif lines[j][0] == '0x0B':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+			
+			elif lines[j][0] == '0x0C':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+
+			elif lines[j][0] == '0x0D':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+
+			elif lines[j][0] == '0x0E':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+
+			elif lines[j][0] == '0x0F':
+				instruction[0][:]=f"{eval(lines[j][0]):04b}"
+
+			print(instruction)
 			instruction = [[],[],[],[],[],[],[],[]]
-			for j in range(0,len(lines[i])):
-				pass
-		
 		print(lines)	
 		with open(file2, 'w', encoding='utf-8') as f1:
 			current_line=f""
